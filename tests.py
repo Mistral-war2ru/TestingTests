a95a2e881362199e3a6d0cd31fe3f5dcfab4608d
@EduardGorodetzkiy <xxxyyyzzzed@gmail.com>
2025-12-10 11:30:21 +0300

    commit

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+import requests
+
+url = "http://127.0.0.1:8000"
+
+class AppTests(unittest.TestCase):
+
+	def test_get_hello_endpoint(self):
+		r = requests.get(url+'/hello')
+		self.assertEqual(json.loads(r.text)['message'], 'Hello world!')
+		
+	def test_post_hello_endpoint(self):
+		r = requests.get(url+'/hello')
+		self.assertEqual(r.status_code, 200)
+		
+if __name__ == '__main__':
+    unittest.main()
